b25224155907b3383480a18b27a738a09b2531eb
root <root@localhost.localdomain>
2014-05-13 23:22:09 +0900

    update

--- switch_application_lib/django_lib/net_builder/MyProgram+builder_urls.py
+++ switch_application_lib/django_lib/net_builder/MyProgram+builder_urls.py
@@ -4,5 +4,6 @@ from net_builder.MyProgram.builder_for_switch_configuration import *
 
 builder_urls = {
  'KR1B_CS_T2_7050S_NEX':KR1B_CS_T2_7050S_NEX,
+ 'KR1B_CS_T2_7050S_NETAPP':KR1B_CS_T2_7050S_NETAPP,
 }
 
